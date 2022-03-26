2663ee61a18901879f4c6384444d511f289d3b78
iamyeasirarafat <yas.yeasir@gmail.com>
2022-03-26 23:14:51 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import HowReactWork from './components/HowReactWork/HowReactWork';
+import PropsVsState from './components/PropsVsState/PropsVsState';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div >
       <Shop></Shop>
+      <HowReactWork></HowReactWork>
+      <PropsVsState></PropsVsState>
     </div>
   );
 }
